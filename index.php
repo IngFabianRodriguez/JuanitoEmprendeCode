7bf9035e2068d4caa0ca51f4e101fc4dd322f578
Unknown <hrodrigue26@uniminuto.edu.co>
2017-10-13 00:07:30 -0500

    Creacion login  con validacion y redireccion

--- index.php
+++ index.php
@@ -8,22 +8,51 @@
     <link rel="stylesheet" href="css/bootstrap.css">
     <!-- fuentes -->
     <link rel="stylesheet" href="css/font-awesome.min.css">
-
+    <title>Home</title>
   </head>
   <body class="fondo">
       <section id="navbar">
-
       <div class="row-fluid">
-          <nav class="text-center navbar navbar-inverse navbar-toggleable-md navbar-light bg-inverse">
+        <nav class="text-center navbar navbar-inverse navbar-toggleable-md navbar-light bg-inverse">
+        <div class="col-lg-6">
             <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbar" aria-controls="navbar" aria-expanded="false" aria-label="Toggle navigation">
               <span class="navbar-toggler-icon"></span>
             </button>
-            <a class="navbar-brand" href="index.html">Juanito Emprende</a>
-          </nav>
+            <a class="navbar-brand" href="index.php">Juanito Emprende</a>
+          </div>
+          <div class="col-lg-6">
+            <form class="form-inline" action="validacion.php" method="post">
+              <input class="form-control mr-sm-2" type="email" name="usuario" pattern="[a-zA-Z0-9_]+([.][a-zA-Z0-9_]+)*@[a-zA-Z0-9_]+([.][a-zA-Z0-9_]+)*[.][a-zA-Z]{1,5}" placeholder="Correo" required/>
+              <input class="form-control mr-sm-2" type="password" name="contraseña" placeholder="Contraseña">
+              <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Ingresar</button>
+            </form>
+          </div>
+        </div>
+      </nav>
+      </section>
+      <section id="jumbotron">
+        <div class="row">
+
+            <div class="jumbotron">
+              <div class="row">
+              <div class="col-lg-1">&nbsp;</div>
+              <div class="col-lg-5">
+                <h2>Señor usuario a continuación podra ingresar el numero del proceso a consultar su estado</h2>
+              </div>
+              <div class="col-lg-1">&nbsp;</div>
+              <div class="col-lg-4">
+                <form action="consulta.php" method="post">
+                    <input class="form-control" type="text" name="contraseña" placeholder="Codigo de Sticker" required/>
+                    <div class="col-lg-12">&nbsp;</div>
+                    <button class="btn btn-success my-2 my-sm-0" type="submit">Ingresar</button>
+                </form>
+              </div>
+              <div class="col-lg-1">&nbsp;</div>
+            </div>
+
+          </div>
         </div>
       </section>
-
-
     <script src="js/jquery.min.js"></script>
     <script src="js/bootstrap.min.js"></script>
   </body>
--- validacion.php
+++ validacion.php
@@ -8,49 +8,25 @@ include ('conexion.php');
   $p=$_REQUEST["contraseña"];
 
 
-  $sql="select * from usuario, login where login.Correo='$u' and login.Password='$p' and usuario.Correo=login.Correo;";
+  $sql="select * from usuario, login where login.Correo='$u' and login.Contrasenia='$p' and usuario.Correo=login.Correo;";
   $result=mysqli_query($link,$sql) or die ("ERROR en la Consulta $sql".mysqli_error($link));
 
   if($row=mysqli_fetch_array($result)){
 
-        $_SESSION['correo']=$row['Correo'];
-        $_SESSION['nombre']=$row['Nombre'];
-        $_SESSION['apellido']=$row['Apellido'];
+        $_SESSION['nombre']=$row['Nombres'];
+        $_SESSION['apellido']=$row['Apellidos'];
         $_SESSION['cargo']=$row['Cargo'];
-        $_SESSION['IdUsuario']=$row['IdUsuario'];
 
-
-if ($p == "Uniminuto") {
   echo "<script type='text/javascript'>
     alert('Bienvenido(a) ".$_SESSION['cargo']." ".$_SESSION['nombre']." ".$_SESSION['apellido']. " al sistema');
-    window.location='/Calle90/usuarios/changepass.html';
+    window.location='indexf.php';
   </script>";
 
-}elseif($p != "Uniminuto" && $_SESSION['cargo']=='Laboratorista') {
-  	echo "<script type='text/javascript'>
-  		alert('Bienvenido(a) ".$_SESSION['cargo']." ".$_SESSION['nombre']." ".$_SESSION['apellido']. " al sistema');
-      window.location='/Calle90/administradores/indexadmon.php';
-  	</script>";
-  }elseif($p != "Uniminuto" && $_SESSION["cargo"]=='Coordinador'){
-    echo "<script type='text/javascript'>
-      alert('Bienvenido(a) ".$_SESSION['cargo']." ".$_SESSION['nombre']." ".$_SESSION['apellido']. " al sistema');
-      window.location='/Calle90/administradores/indexadmon.php';
-    </script>";
-  }elseif($p != "Uniminuto" && $_SESSION["cargo"]=='Desarrollador'){
-    echo "<script type='text/javascript'>
-      alert('Bienvenido(a) ".$_SESSION['cargo']." ".$_SESSION['nombre']." ".$_SESSION['apellido']. " al sistema');
-      window.location='/Calle90/administradores/indexadmon.php';
-    </script>";
-  }
-
-} else{
- echo "<script type='text/javascript'>
-    alert('El usuario o el pass ingresados no conciden');
-      window.location='/Calle90/index.php';
+}else {
+  echo "<script type='text/javascript'>
+    alert('Ingrese de nuevo usuario y contraseña al sistema');
+    window.location='index.php';
   </script>";
 }
-
-
-
 
 ?>
