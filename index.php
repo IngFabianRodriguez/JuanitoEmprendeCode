c8d555e12471b6675f9e93f8600802b5947bff7d
Unknown <hrodrigue26@uniminuto.edu.co>
2017-10-12 21:17:29 -0500

    Insercion de esqueleto base de codigo

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <!-- fuentes -->
+    <link rel="stylesheet" href="css/font-awesome.min.css">
+
+  </head>
+  <body>
+    <script type="text/javascript">
+    var a =0;
+      if(a==0):alert(a);
+    </script>
+    <!-- jQuery first, then Tether, then Bootstrap JS. -->
+    <script src="js/jquery.min.js"></script>
+
+    <script src="js/bootstrap.min.js"></script>
+  </body>
+</html>
