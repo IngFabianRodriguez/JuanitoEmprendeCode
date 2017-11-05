af3dfc15768f1dc2f70b69ed1e702f9889e91e97
Unknown <hrodrigue26@uniminuto.edu.co>
2017-11-05 00:16:01 -0500

    UpdateBandeja patinador

---
+++ Patinador/bandeja.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="es">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="../css/bootstrap.css">
+    <!-- Estilos -->
+    <link rel="stylesheet" href="../css/estilos.css">
+    <!-- fuentes -->
+    <link rel="stylesheet" href="../css/font-awesome.min.css">
+
+  </head>
+  <body>
+
+    <section id="navbar">
+      <div class="row-fluid">
+        <nav class="text-center navbar navbar-inverse navbar-toggleable-md navbar-light bg-inverse">
+          <div class="col-lg-6">
+            <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbar" aria-controls="navbar" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+              </button>
+            <a class="navbar-brand" href="index.php">Juanito Emprende</a>
+          </div>
+          <div class="col-lg-6 text-white">
+          <?php
+          session_start();
+          include '../inc/operaciones.php';
+          imprimirNombres();
+            ?>
+            <span>&nbsp;</span>
+            <span>&nbsp;</span>
+            <span>&nbsp;</span>
+            <span>&nbsp;</span>
+            <span>&nbsp;</span>
+  <a href="../inc/salir.php" class="btn btn-outline-danger my-2 my-sm-0"><i class="fa fa-sign-out" aria-hidden="true"></i>Cerrar Sesion <?php echo $_SESSION['cargo'];?></a>
+          </div>
+      </div>
+      </nav>
+    </section>
+<section>
+  <div class="jumbotron">
+    <H3 class="text-center">Lista de documentos a entregar segun hora de entrada</H3>
+  </div>
+</section>
+</body>
+</html>
