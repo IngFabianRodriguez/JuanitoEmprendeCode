531888ce4c95d8b83757f851cfb6b6ecd1466497
Unknown <hrodrigue26@uniminuto.edu.co>
2017-11-13 00:25:47 -0500

    Soluciona pdf diferente por asiganacion de horas patinador

--- Patinador/ListaDoc.php
+++ Patinador/ListaDoc.php
@@ -2,6 +2,7 @@
 session_start();
 require '../inc/fpdf/fpdf.php';
 include '../inc/conexion.php';
+include '../inc/operaciones.php';
 $link=conectar();
 
 $pdf= new FPDF();
@@ -15,35 +16,54 @@ $pdf->SetFont('Arial','',9);
 $pdf->Cell(50,15,'  Fecha generacion: '.date('Y-m-d').'',1);
 $pdf->Ln(18);
 //Consulta
+imprimirhora();
+$fechaActual=date('Y-m-d');
 
-
-/*$sql='SELECT * FROM proveedor WHERE NIT= "'.$_SESSION['NIT'].'"';
-$result=mysqli_query($link,$sql) or die ("ERROR en la Consulta $sql".mysqli_error($link));
-if($result->num_rows>0){
-   while($r=$result->fetch_array()){
-$pdf->Cell(100,8,'',0,'C');
-$pdf->Cell(40,8,'Fecha de creacion',1,0,'C');
-$pdf->Cell(50,8,$r['FechaCreacion'],1,0,'C');
-$pdf->Ln(12);
-$pdf->SetFont('Arial','B',10);
-$pdf->Cell(190,8," 1. Datos basicos del Proveedor",1,0,'C');
-$pdf->Ln(12);
-$pdf->SetFont('Arial','',10);
-
-
-$pdf->Cell(45,8,'Razon Social:',1,0,'C');
-$pdf->Cell(50,8,$r['RazonSocial'],1,0,'C');
-$pdf->Cell(45,8,'NIT:',1,0,'C');
-$pdf->Cell(50,8,$r['NIT'],1,0,'C');
-$pdf->Ln(8);
-$pdf->Cell(45,8,'Telefono:',1,0,'C');
-$pdf->Cell(50,8,$r['Telefono'],1,0,'C');
-$pdf->Cell(45,8,'Correo:',1,0,'C');
-$pdf->Cell(50,8,$r['Correo'],1,0,'C');
-$pdf->Ln(8);
-   }
-
- }*/
+if ($_SESSION['hora']<='11:59:59') {
+  $sql='SELECT * FROM `documento` WHERE Hora_ingreso BETWEEN "08:00:00" AND "11:59:00" AND Fecha_ingreso = "'.date('Y-m-d').'"';
+  $result=mysqli_query($link,$sql) or die ("ERROR en la Consulta $sql".mysqli_error($link));
+  if($result->num_rows>0){
+     while($r=$result->fetch_array()){
+  $pdf->Cell(100,8,'',0,'C');
+  $pdf->Cell(40,8,'Fecha de Entrega',1,0,'C');
+  $pdf->Cell(50,8,$r['Fecha_ingreso'],1,0,'C');
+  $pdf->Ln(12);/*
+  $pdf->Cell(45,8,'Razon Social:',1,0,'C');
+  $pdf->Cell(50,8,$r['RazonSocial'],1,0,'C');
+  $pdf->Cell(45,8,'NIT:',1,0,'C');
+  $pdf->Cell(50,8,$r['NIT'],1,0,'C');
+  $pdf->Ln(8);
+  $pdf->Cell(45,8,'Telefono:',1,0,'C');
+  $pdf->Cell(50,8,$r['Telefono'],1,0,'C');
+  $pdf->Cell(45,8,'Correo:',1,0,'C');
+  $pdf->Cell(50,8,$r['Correo'],1,0,'C');
+  $pdf->Ln(8);
+  */
+     }
+}
+}elseif ($_SESSION['hora']>='12:00:00' && $_SESSION['hora']<='14:59:00') {
+  $sql='SELECT * FROM `documento` WHERE Hora_ingreso BETWEEN "11:59:01" AND "14:59:01" AND Fecha_ingreso = "'.date('Y-m-d').'"';
+  $result=mysqli_query($link,$sql) or die ("ERROR en la Consulta $sql".mysqli_error($link));
+  if($result->num_rows>0){
+     while($r=$result->fetch_array()){
+  $pdf->Cell(100,8,'',0,'C');
+  $pdf->Cell(40,8,'Fecha de Entrega',1,0,'C');
+  $pdf->Cell(50,8,$r['Fecha_ingreso'],1,0,'C');
+  $pdf->Ln(12);/*
+  $pdf->Cell(45,8,'Razon Social:',1,0,'C');
+  $pdf->Cell(50,8,$r['RazonSocial'],1,0,'C');
+  $pdf->Cell(45,8,'NIT:',1,0,'C');
+  $pdf->Cell(50,8,$r['NIT'],1,0,'C');
+  $pdf->Ln(8);
+  $pdf->Cell(45,8,'Telefono:',1,0,'C');
+  $pdf->Cell(50,8,$r['Telefono'],1,0,'C');
+  $pdf->Cell(45,8,'Correo:',1,0,'C');
+  $pdf->Cell(50,8,$r['Correo'],1,0,'C');
+  $pdf->Ln(8);
+  */
+     }
+}
+}
 //$pdf->Cell();
 $pdf->Output();
   ?>
